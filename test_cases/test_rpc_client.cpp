55a85084198e66f1d6db3bfd283acfffcaa3f774
HeYaoting666 <2077104031@qq.com>
2024-03-14 16:02:04 +0800

    UpDate

--- test_cases/test_rpc_client.cpp
+++ test_cases/test_rpc_client.cpp
@@ -10,19 +10,22 @@ int main() {
     rocket::Config::SetGlobalConfig("./conf/rocket_client.xml");
     rocket::Logger::InitGlobalLogger(0);
 
+    // 定义rpc channel
     rocket::IPNetAddr::spointer addr = std::make_shared<rocket::IPNetAddr>("127.0.0.1", 12310);
     auto channel = std::make_shared<rocket::RpcChannel>(addr);
 
+    // 定义消息体 message
     auto request = std::make_shared<makeOrderRequest>();
     auto response = std::make_shared<makeOrderResponse>();
-
     request->set_price(100);
     request->set_goods("apple");
 
+    // 定义rpc controller
     auto controller = std::make_shared<rocket::RpcController>();
     controller->SetMsgId("99998888"); // 设置 msg_id
     controller->SetTimeout(5000);            // 设置超时时间为 5s
 
+    // 定义rpc closure
     std::shared_ptr<rocket::RpcClosure> closure = std::make_shared<rocket::RpcClosure>([&channel, request, response, controller]() {
         if (controller->GetErrorCode() == 0) {
             INFOLOG("call rpc success, request[%s], response[%s]", request->ShortDebugString().c_str(), response->ShortDebugString().c_str())
@@ -43,6 +46,7 @@ int main() {
 
     // 调用 rpc 服务
     channel->Init(controller, request, response, closure);
+    // 调用rpc channel CallMethod方法
     Order_Stub(channel.get()).makeOrder(controller.get(), request.get(), response.get(), closure.get());
 
     return 0;
